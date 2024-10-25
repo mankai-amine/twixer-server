9919292d0a4a3ba29b7dc8627ec6281bf8419599
SDarbyson <Sean.Darbyson@johnabbottcollege.net>
2024-10-25 11:33:29 -0400

    merge main, conforming to dif style

--- config/database.js
+++
@@ -1,8 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize('TOFIX', 'REDACTED', 'REDACTED', {
-    host: 'localhost',
-    dialect: 'mysql',
-});
-
-module.exports = sequelize;
