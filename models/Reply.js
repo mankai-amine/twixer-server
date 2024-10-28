391d54953eef6bc5505848fb705831203104a426
SDarbyson <Sean.Darbyson@johnabbottcollege.net>
2024-10-28 12:30:14 -0400

    setup of reply model

--- models/Reply.js
+++ models/Reply.js
@@ -3,16 +3,28 @@ const sequelize = require('../config/database');
 const User = require('./User');
 const Post = require('./Post');
 const Like = require('./Like');
-const Reply = require('./Reply');
 
-class Post extends Model {}
+class Reply extends Model {}
 
-Post.init(
+Reply.init(
     {
         id: {
             type: DataTypes.INTEGER,
             autoIncrement: true,
             primaryKey: true,
+        },
+        post_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: Post,
+                key: 'id',
+            },
+        },
+        date: {
+            type: DataTypes.DATE,
+            defaultValue: DataTypes.NOW,
+            allowNull: false,
         },
         user_id: {
             type: DataTypes.INTEGER,
@@ -22,33 +34,23 @@ Post.init(
                 key: 'id',
             },
         },
-        date: {
-            type: DataTypes.DATE,
-            defaultValue: DataTypes.NOW,
-            allowNull: false,
-        },
         content: {
-            type: DataTypes.STRING(560),
+            type: DataTypes.STRING(280),
             allowNull: false,
         },
-        is_deleted: {
-            type: DataTypes.BOOLEAN,
-            allowNull: false,
-            defaultValue: false,
-        },
-        orig_post_id: {
+        orig_reply_id: {
             type: DataTypes.INTEGER,
             allowNull: true,
             references: {
-                model: Post,
+                model: Reply,
                 key: 'id',
             },
         }
     }
 );
 
-Post.belongsTo(User, {foreignKey: 'user_id' });
-Post.hasMany(Like, {foreignKey: 'post_id' });
-Post.hasMany(Reply, {foreignKey: 'post_id'});
+Reply.belongsTo(User, {foreignKey: 'user_id' });
+Reply.belongsTo(Post, {foreignKey: 'post_id' });
+Reply.hasMany(Like, {foreignKey: 'reply_id' });
 
-module.exports = Post;
+module.exports = Reply;
