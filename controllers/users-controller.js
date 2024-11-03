eaa3d8aab0769c39288122cfd9b3087093c9cd9a
Michael <160180509+m1ndb0x@users.noreply.github.com>
2024-11-03 12:19:28 -0500

    pagination changes

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -245,14 +245,6 @@ module.exports = {
         }
     },
     getAllUsers: async (req, res) => {
-        // try {
-        //     const users = await User.findAll();
-        //     res.status(200).json(users);
-        // } catch (error) {
-        //     console.error(error);
-        //     res.status(500).json({ error: 'Internal server error' });
-        // }
-
         try {
             const page = parseInt(req.query.page) || 1;
             const limit = parseInt(req.query.limit) || 10;
