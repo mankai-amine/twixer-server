0e4635a9bc51218d9ed1e80bee09b6785b0c6b78
Michael <160180509+m1ndb0x@users.noreply.github.com>
2024-11-04 08:24:37 -0500

    heroku set

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const express = require("express");
 const app = express();
 const cors = require('cors');  
 
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
-//app.use(cors({ origin: 'https://twixer-client-7fc12e0d4cd5.herokuapp.com', credentials: true }));
+// app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(cors({ origin: 'https://twixer-client-7fc12e0d4cd5.herokuapp.com', credentials: true }));
 
 app.use(express.json());
 
